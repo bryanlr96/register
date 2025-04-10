9ef931cf7b655cf0042b304f5b7af1b8cabf15e8
bryanlr96 <bryanlopezruiz1996@gmail.com>
2025-04-10 18:05:24 +0200

    creando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
+  const [books, setBooks] = useState([])
+
+  useEffect(()=>{
+    const initalBooks = localStorage.getItem('books')
+    if(initalBooks){
+      setBooks(JSON.parse(initalBooks))
+    }
+    console.log(books)
+  },[])
 
   return (
     <>
---
+++ src/components/tableItem/TableItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TableItem() {
+  return (
+    <div>TableItem</div>
+  )
+}
